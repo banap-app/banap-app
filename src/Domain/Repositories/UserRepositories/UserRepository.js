3afbcb53e7b7bf0131ba3398b4b631d7d2cf5a6e
Asher Novelli <asherndebortoli@gmail.com>
2024-03-15 22:53:42 -0300

    Add verify all methods are implemented

--- src/Domain/Repositories/UserRepositories/UserRepository.js
+++ src/Domain/Repositories/UserRepositories/UserRepository.js
@@ -3,13 +3,27 @@ import User from '../../Entities/User.js'
 import DomainException from '../../Exceptions/DomainException.js'
 
 export default class UserRepository extends Abstract {
-  save (user) {
+  save(user) {
     if (user instanceof User) throw new DomainException('Must be a User')
     return user
   }
 
-  update (user) {
+  update(user) {
     if (user instanceof User) throw new DomainException('Must be a User')
     return user
   }
 }
+
+class Tester extends UserRepository {
+  save(user) {
+    super.save(user)
+  }
+
+  update(user) {
+    super.update(user)
+
+    return user
+  }
+}
+
+let t = new Tester()
--- src/__seedwork/Domain/Abstract.js
+++ src/__seedwork/Domain/Abstract.js
@@ -8,12 +8,23 @@ export default class Abstract {
    * Cria uma instância de Abstract.
    * @throws {AbstractError} Lança uma exceção se esta classe for instanciada diretamente.
    */
-  constructor () {
+  constructor() {
     if (new.target === Abstract) {
       throw new AbstractError('Cannot instantiate abstract class directly.')
     }
     if (Object.getPrototypeOf(new.target.prototype).constructor === Abstract) {
       throw new AbstractError('Cannot instantiate abstract class directly.')
     }
+
+    const classExtends = Object.getOwnPropertyNames(this.constructor.prototype)
+    const classNative = Object.getOwnPropertyNames(
+      Object.getPrototypeOf(new.target.prototype)
+    )
+
+    classNative.forEach((method) => {
+      if (classExtends.indexOf(method) === -1) {
+        throw new AbstractError('Need implement all methods of Entity')
+      }
+    })
   }
 }
