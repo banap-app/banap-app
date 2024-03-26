674ba283cd211707a65bc40f71901d5dcaaea01c
Asher Novelli <asherndebortoli@gmail.com>
2024-03-25 22:17:41 -0300

    test: add test object equals for user

--- src/Tests/Domain/Entities/User.test.js
+++ src/Tests/Domain/Entities/User.test.js
@@ -1,4 +1,10 @@
+import { v4 } from "uuid"
 import User from "../../../Domain/Entities/User"
+
+jest.mock('uuid', () => ({
+  v4: jest.fn(() => 'd5afc26b-eb03-452d-b855-de51c811d691'), // UUID válido
+  validate: jest.fn(() => true) // Sempre retorna true para a validação
+}))
 
 describe('User Tests', () => {
   test('should first', () => {
@@ -20,5 +26,18 @@ describe('User Tests', () => {
     const user = new User('Asher', 'Novelli#2@as', 'asher@gmail.com', '', false)
     user.activate()
     expect(user.get('active')).toBe(true)
+  })
+
+  test('', () => {
+    
+    const user = new User('Asher', 'Novelli#2@as', 'asher@gmail.com', '', false)
+    expect(user.to_dict()).toMatchObject({
+      id: 'd5afc26b-eb03-452d-b855-de51c811d691',
+      name: 'Asher',
+      password: 'Novelli#2@as',
+      email: 'asher@gmail.com',
+      created_at: expect.any(Date),
+      active: false
+    })
   })
 })
