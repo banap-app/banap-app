095ed132508238f118ca078d594740c7ae490c6e
Asher Novelli <asherndebortoli@gmail.com>
2024-03-27 21:53:29 -0300

    Start Front-end

--- src/__seedwork/Infra/Controller.js
+++
@@ -1,8 +0,0 @@
-import Abstract from '../Abstract.js'
-import AbstractError from '../AbstractError.js'
-
-export default class Controller extends Abstract {
-  execute () {
-    throw new AbstractError('Not implemented')
-  }
-}
