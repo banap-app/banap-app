da04e8db68789459864bef37621258d3810154d5
Victor Luís <victorluiscaguiar@gmail.com>
2024-04-26 03:12:52 -0300

    Added tailwindcss to the project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
-    'standard'
+    'standard',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
@@ -17,11 +17,10 @@ module.exports = {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
-      
     ],
-    "no-unneeded-ternary":0,
-    "no-unused-vars":"warn",
-    "prefer-regex-literals":0,
-    "no-useless-escape":"off"
+    'no-unneeded-ternary': 0,
+    'no-unused-vars': 'warn',
+    'prefer-regex-literals': 0,
+    'no-useless-escape': 'off',
   },
 }
